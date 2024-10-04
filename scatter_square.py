c419e9fdf6e55b8e8d08e8f698bee0512eb22c8d
Jen Kuenning <jkue@loc.gov>
2024-10-04 16:44:44 -0400

    initial code

---
+++ scatter_square.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+# ax.scatter(2, 4, s=200)  # s=# dot size
+ax.scatter(x_values, y_values, s=100)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# set size of tick labels.
+ax.tick_params(labelsize=14)
+
+plt.show()
