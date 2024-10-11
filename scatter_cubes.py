77526495deede79c75ddf3920f52102389a0c1a9
Jen Kuenning <jkue@loc.gov>
2024-10-11 15:21:41 -0400

    fix cube add random walk

--- scatter_cubes.py
+++ scatter_cubes.py
@@ -7,20 +7,21 @@ y_values = [x**3 for x in x_values]
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
 
 # set size of tick labels.
 ax.tick_params(labelsize=14)
 
 # Set the range for each axis.
-# ax.ticklabel_format(style='plain')
+
 # ax.axis([0, 6, 0, 130])
-ax.axis([0, 810, 0, 50_500_000])
+ax.axis([0, 1000, 0, 999_999_999])
+ax.ticklabel_format(style='plain')
 
 plt.show()
 # plt.savefig('squares_cube_5k.jpg', bbox_inches='tight')
